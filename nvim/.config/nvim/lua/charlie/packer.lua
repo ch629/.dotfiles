a34bae14b38cc5da12fbea7cb9d1554bda24ff59
ch629 <charliehowes97@outlook.com>
2021-11-07 19:49:20 +0000

    fix: moved packer install path to fix compile/update issues.

--- nvim/.config/nvim/lua/charlie/packer.lua
+++ nvim/.config/nvim/lua/charlie/packer.lua
@@ -1,17 +1,14 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local fn = vim.fn local install_path = fn.stdpath('data') ..
+    '/site/pack/packer/opt/packer.nvim'
 
-if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path})
+if fn.empty(fn.glob(install_path)) > 0 then fn.system({'git', 'clone',
+    '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 vim.cmd('packadd packer.nvim')
-local use = require'packer'.use
 
-return require'packer'.startup(function ()
-    use { 'lewis6991/impatient.nvim', run = function ()
-        require'impatient'
-    end }
+return require'packer'.startup(function (use)
+    use { 'lewis6991/impatient.nvim', run = function() require'impatient' end }
 
     -- Theme
     use 'Pocco81/Catppuccino.nvim'
