e73f21d5f31eb657352cc93e6dea0301aa15b71e
ch629 <charliehowes97@outlook.com>
2021-11-07 21:22:40 +0000

    feat: using impatient with packer_compiled to speed boot time.

--- nvim/.config/nvim/lua/charlie/init.lua
+++ nvim/.config/nvim/lua/charlie/init.lua
@@ -1,2 +1,4 @@
+require'impatient'
 require'charlie.packer'
+require'packer_compiled'
 require'charlie.options'
--- nvim/.config/nvim/lua/charlie/packer.lua
+++ nvim/.config/nvim/lua/charlie/packer.lua
@@ -6,97 +6,106 @@ if fn.empty(fn.glob(install_path)) > 0 then fn.system({'git', 'clone',
 end
 
 vim.cmd('packadd packer.nvim')
+local use = require'packer'.use
 
-return require'packer'.startup(function (use)
-    use { 'lewis6991/impatient.nvim', run = function () require'impatient' end }
+return require'packer'.startup({
+    function()
+        use {
+            'lewis6991/impatient.nvim',
+            run = function () require'impatient' end,
+        }
 
-    -- Libraries
-    use 'nvim-lua/plenary.nvim'
+        -- Libraries
+        use 'nvim-lua/plenary.nvim'
 
-    -- NerdTree
-    use {
-        'preservim/nerdtree',
-        config = function() require'charlie.nerdtree' end,
-    }
+        -- NerdTree
+        use {
+            'preservim/nerdtree',
+            config = function() require'charlie.nerdtree' end,
+        }
 
-    -- Status line
-    use {
-        'nvim-lualine/lualine.nvim',
-        config = function() require'charlie.statusline' end,
-    }
+        -- Status line
+        use {
+            'nvim-lualine/lualine.nvim',
+            config = function() require'charlie.statusline' end,
+        }
 
-    -- Git
-    use 'tpope/vim-rhubarb'
-    use 'tpope/vim-fugitive'
-    use 'airblade/vim-gitgutter'
+        -- Git
+        use 'tpope/vim-rhubarb'
+        use 'tpope/vim-fugitive'
+        use 'airblade/vim-gitgutter'
 
-    -- Telescope
-    use {
-        'nvim-telescope/telescope.nvim',
-        config = function() require'charlie.telescope' end,
-    }
-    use 'nvim-telescope/telescope-fzy-native.nvim'
+        -- Telescope
+        use {
+            'nvim-telescope/telescope.nvim',
+            config = function() require'charlie.telescope' end,
+        }
+        use 'nvim-telescope/telescope-fzy-native.nvim'
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        config = function() require'charlie.treesitter' end,
-        run = ':TSUpdate',
-    }
-    use 'nvim-treesitter/playground'
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            config = function() require'charlie.treesitter' end,
+            run = ':TSUpdate',
+        }
+        use 'nvim-treesitter/playground'
 
-    use 'jiangmiao/auto-pairs'
-    use 'dense-analysis/ale'
-    use {
-        'lukas-reineke/indent-blankline.nvim',
-        config = function() require'charlie.indent_blankline' end,
-    }
+        use 'jiangmiao/auto-pairs'
+        use 'dense-analysis/ale'
+        use {
+            'lukas-reineke/indent-blankline.nvim',
+            config = function() require'charlie.indent_blankline' end,
+        }
 
-    -- Util
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-surround'
+        -- Util
+        use 'tpope/vim-commentary'
+        use 'tpope/vim-surround'
 
-    use {
-        'folke/which-key.nvim',
-        config = function() require'which-key'.setup{} end,
-    }
+        use {
+            'folke/which-key.nvim',
+            config = function() require'which-key'.setup{} end,
+        }
 
-    -- LSP
-    use {
-        'neovim/nvim-lspconfig',
-        config = function() require'charlie.lsp' end,
-    }
-    use 'williamboman/nvim-lsp-installer'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'folke/trouble.nvim'
-    use 'folke/todo-comments.nvim'
-    use 'glepnir/lspsaga.nvim'
+        -- LSP
+        use {
+            'neovim/nvim-lspconfig',
+            config = function() require'charlie.lsp' end,
+        }
+        use 'williamboman/nvim-lsp-installer'
+        use 'kyazdani42/nvim-web-devicons'
+        use 'folke/trouble.nvim'
+        use 'folke/todo-comments.nvim'
+        use 'glepnir/lspsaga.nvim'
 
-    -- Completions
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/vim-vsnip'
+        -- Completions
+        use 'hrsh7th/cmp-nvim-lsp'
+        use 'hrsh7th/cmp-buffer'
+        use 'hrsh7th/nvim-cmp'
+        use 'hrsh7th/cmp-vsnip'
+        use 'hrsh7th/vim-vsnip'
 
-    -- Go
-    use {
-        'fatih/vim-go',
-        run = ':GoInstallBinaries',
-    }
-    use 'sebdah/vim-delve'
+        -- Go
+        use {
+            'fatih/vim-go',
+            run = ':GoInstallBinaries',
+        }
+        use 'sebdah/vim-delve'
 
-    -- Theme
-    use {
-        'Pocco81/Catppuccino.nvim',
-        config = function()
-            require'charlie.catppuccino'
-            vim.cmd('colorscheme catppuccino')
-        end,
-    }
+        -- Theme
+        use {
+            'Pocco81/Catppuccino.nvim',
+            config = function()
+                require'charlie.catppuccino'
+                vim.cmd('colorscheme catppuccino')
+            end,
+        }
 
-    -- Keybindings
-    use {
-        'LionC/nest.nvim',
-        config = function() require'charlie.bindings' end,
-    }
-end)
+        -- Keybindings
+        use {
+            'LionC/nest.nvim',
+            config = function() require'charlie.bindings' end,
+        }
+    end,
+    config = {
+        compile_path = fn.stdpath('config') .. '/lua/packer_compiled.lua',
+    },
+})
