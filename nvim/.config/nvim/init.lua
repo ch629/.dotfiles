5a07ef9c95faad80e3970d6ba0aada9953f073ac
ch629 <charliehowes97@outlook.com>
2021-11-10 10:03:22 +0000

    vimscript be gone.

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,33 @@
+require'impatient'
+require'charlie'
+
+vim.cmd[[
+    cnoreabbrev W! w!
+    cnoreabbrev Q! q!
+    cnoreabbrev Qall! qall!
+    cnoreabbrev Wq wq
+    cnoreabbrev Wa wa
+    cnoreabbrev wQ wq
+    cnoreabbrev WQ wq
+    cnoreabbrev W w
+    cnoreabbrev Q q
+    cnoreabbrev Qall qall
+    cnoreabbrev Qa qa
+    cnoreabbrev qA qa
+
+    " remove trailing whitespaces
+    command! FixWhitespace :%s/\s\+$//e
+    " Format JSON using jq
+    command! JsonFmt :%!jq '.'
+
+    augroup vimrc-remember-cursor-position
+      autocmd!
+      autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+    augroup END
+
+    if has('macunix')
+      " pbcopy for OSX copy/paste
+      vmap <C-x> :!pbcopy<CR>
+      vmap <C-c> :w !pbcopy<CR><CR>
+    endif
+]]
--- nvim/.config/nvim/lua/charlie/init.lua
+++ nvim/.config/nvim/lua/charlie/init.lua
@@ -1,4 +1,3 @@
-require'impatient'
 require'charlie.packer'
 require'packer_compiled'
 require'charlie.options'
