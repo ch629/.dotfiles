a4ae97123d2b78be8cb1e550bd4018834e343897
ch629 <charliehowes97@outlook.com>
2021-11-19 20:09:47 +0000

    fix: using lsp-installer for sumneko_lua setup.

--- nvim/.config/nvim/lua/charlie/lsp/lua.lua
+++ nvim/.config/nvim/lua/charlie/lsp/lua.lua
@@ -1,27 +1,30 @@
-local sumneko_binary_path = vim.fn.exepath('lua-language-server')
-local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
+local lsp_installer_servers = require'nvim-lsp-installer.servers'
 
-require'lspconfig'.sumneko_lua.setup {
-    cmd = {sumneko_binary_path, '-E', sumneko_root_path .. '/main.lua'};
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT',
-                path = vim.split(package.path, ';'),
-            },
-            diagnostics = {
-                globals = {'vim'},
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+local server_available, requested_server = lsp_installer_servers.get_server('sumneko_lua')
+if server_available then
+    requested_server:on_ready(function() 
+        requested_server:setup {
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = 'LuaJIT',
+                        path = vim.split(package.path, ';'),
+                    },
+                    diagnostics = {
+                        globals = {'vim'},
+                    },
+                    workspace = {
+                        library = {
+                            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                        },
+                    },
+                    telemetry = {
+                        enable = false,
+                    },
                 },
             },
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-    capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-}
+            capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        }
+    end)
+end
