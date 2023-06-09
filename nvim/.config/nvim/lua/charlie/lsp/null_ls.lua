3c78fb43c89d5a1938d036da6e07e24251cc7177
Charlie Howes <charlie@banked.com>
2023-06-09 10:32:03 +0100

    chore: update to new null ls formatting

--- nvim/.config/nvim/lua/charlie/lsp/null_ls.lua
+++ nvim/.config/nvim/lua/charlie/lsp/null_ls.lua
@@ -1,17 +1,23 @@
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
 	timeout_ms = 5000,
 	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 	debug = false,
-	on_attach = function(client)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.cmd([[
-                augroup LspFormatting
-                    autocmd! * <buffer>
-                    autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ timeout_ms = 5000 })
-                augroup END
-            ]])
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						timeout_ms = 5000,
+						async = false,
+					})
+				end,
+			})
 		end
 	end,
 	sources = {
